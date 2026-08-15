43ad685bd1fae5964fe638c8bf24558255f90fa2
m1rage <miragela.code@gmail.com>
2026-08-15 14:58:18 +0200

    feat(lowering): enhance variable declaration transformation

--- crates/lowering/src/transformers/var_decl.rs
+++ crates/lowering/src/transformers/var_decl.rs
@@ -1,5 +1,20 @@
+use schema::declarations::{VariableDeclaration};
 use swc_ecma_ast::VarDecl;
 
-pub(crate) fn transform_var_decl(var: &VarDecl) {
-    // ...
+use crate::transform;
+
+pub(crate) fn transform_var_decl(node: &VarDecl) -> VariableDeclaration {
+
+    for decl in &node.decls {
+        transform(decl);
+    }
+
+    VariableDeclaration {
+        declarations: transformed_declarations,
+        position: Position {
+            line: node.span.lo().line,
+            column: node.span.lo().col,
+        }
+    }
 }
+
--- crates/schema/src/declarations.rs
+++ crates/schema/src/declarations.rs
@@ -1,7 +1,16 @@
 use crate::{Position, expressions::Expression, identifiers::Identifier};
 
+pub enum Declarations {
+    VariableDeclaration(VariableDeclaration),
+    VariableDeclarator(VariableDeclarator),
+}
+
 pub struct VariableDeclaration {
-    identifier: Identifier,
-    initializer: Option<Expression>,
-    position: Position,
+    pub declarations: Vec<VariableDeclarator>,
+    pub position: Position,
+}
+
+pub struct VariableDeclarator {
+    pub id: Identifier,
+    pub init: Option<Expression>
 }
